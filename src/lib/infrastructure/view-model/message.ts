c23dcf1aaa1f0d5b21c6698d1fe16ecbccf4212a
maany <imptodefeat@gmail.com>
2023-10-11 00:45:46 +0200

    storybook housekeeping

--- src/lib/infrastructure/view-model/message.ts
+++ src/lib/infrastructure/view-model/message.ts
@@ -1,8 +1,10 @@
+import { MessageType } from "@/lib/core/entities/message";
 import { BaseComponentViewModel } from "./base";
 
 export interface MessageViewModel extends BaseComponentViewModel{
     id: number,
     name: string,
     timestamp: Date,
-    content: string
+    content: string,
+    type: MessageType
 }
