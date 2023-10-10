cb1e06c4cd2b75bebf9707792f666c743ccba650
maany <imptodefeat@gmail.com>
2023-10-11 00:00:39 +0200

    add storybook, taiwind

---
+++ src/sdk/entity.ts
@@ -0,0 +1,32 @@
+interface Typography {}
+interface BasePalette {}
+interface Spacing {}
+interface BaseBreakpoint {}
+interface ZIndex {}
+interface Transitions {}
+interface Component {}
+interface BaseTheme{
+    typography: Typography;
+    palette: BasePalette;
+    spacing: Spacing;
+    breakpoints: BaseBreakpoint;
+    zIndex: ZIndex;
+    transitions: Transitions;
+    components: Component;
+}
+
+interface AccentColorScheme {
+    primaryKey: string;
+    secondaryKey: string;
+    tertiaryKey: string;
+}
+
+interface NeutralColorScheme {
+    neutralVariantKey: string;
+    neutralKey: string;
+}
+
+interface BaseColorScheme {
+    accent: AccentColorScheme;
+    neutral: NeutralColorScheme; 
+}
---
+++ src/sdk/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+        main: "#f44336",
+        },
+    },
+});
