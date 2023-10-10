ea3e9f0acbc83b2633e418fa8dd83adb1e63fc49
maany <imptodefeat@gmail.com>
2023-10-11 00:34:18 +0200

    move sdk to lib/common

--- src/lib/infrastructure/view-model/base.ts
+++ src/lib/infrastructure/view-model/base.ts
@@ -1,6 +1,6 @@
-import { DeviceType } from "@/sdk/entity"
-import { Mode } from "@/sdk/theme"
-import { BaseViewModel } from "@/sdk/view-model"
+import { DeviceType } from "@/lib/common/entity"
+import { Mode } from "@/lib/common/theme"
+import { BaseViewModel } from "@/lib/common/view-model"
 
 export interface BaseComponentViewModel extends BaseViewModel {
     device: DeviceType
