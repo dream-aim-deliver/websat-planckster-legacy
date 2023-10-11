c3954378e9ea39513c5258d83595dc052605ba7e
maany <imptodefeat@gmail.com>
2023-10-11 11:26:19 +0200

    beast backup

--- src/lib/common/theme.ts
+++ src/lib/common/theme.ts
@@ -4,11 +4,56 @@ export enum Mode {
   LIGHT = 'light',
   DARK = 'dark',
 }
+// TODO: this can go in CSS variables in global styles.
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#f44336',
-    },
+const palette = {
+  mode: Mode.LIGHT,
+  primary: {
+    main: '#f44336',
   },
-})
+  secondary: {
+    main: '#3f51b5',
+  },
+  tertiary: {
+    main: '#fff',
+  },
+  neutral: {
+    main: '#000',
+  },
+  neutralVariant: {
+    main: '#fff',
+  },
+}
+
+const darkPalette = {
+  mode: Mode.DARK,
+  primary: {
+    main: '#f44336',
+  },
+  secondary: {
+    main: '#3f51b5',
+  },
+  tertiary: {
+    main: '#fff',
+  },
+  neutral: {
+    main: '#000',
+  },
+  neutralVariant: {
+    main: '#fff',
+  },
+}
+
+export const generateTheme = (mode: Mode) => {
+  return createTheme({
+    palette: mode === Mode.DARK ? darkPalette : palette,
+  })
+}
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#f44336',
+//     },
+//   },
+// })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  // TODO: adjust screen breakpoints, phone, tablet, laptop, desktop, projector
   theme: {
     extend: {
       backgroundImage: {
